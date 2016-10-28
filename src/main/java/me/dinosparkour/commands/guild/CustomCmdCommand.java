f99853eae79e446d4e6fd1a3e090708657fb6638
Dinos <emeraldpmc@gmail.com>
2016-10-28 18:35:28 +0300

    Refactored and added CustomCmd editing & resetting

--- src/main/java/me/dinosparkour/commands/guild/CustomCmdCommand.java
+++ src/main/java/me/dinosparkour/commands/guild/CustomCmdCommand.java
@@ -18,53 +18,14 @@ public class CustomCmdCommand extends GuildCommand {
 
     @Override
     public void executeCommand(String[] args, MessageReceivedEvent e, MessageSender chat) {
-        ServerManager sm = new ServerManager(e.getGuild());
-        String inputArgs = String.join(" ", Arrays.asList(Arrays.copyOfRange(args, 1, args.length)));
         switch (args[0].toLowerCase()) {
             case "create": // Create new command
-                if (isNotAuthorized(chat, e.getAuthor(), e.getGuild())) return;
-                else if (!String.join(" ", Arrays.asList(args)).contains("|")) {
-                    chat.sendUsageMessage();
-                    return;
-                } else {
-                    String name = inputArgs.substring(0, inputArgs.indexOf("|")).trim();
-                    String allResponses = e.getMessage()
-                            .getRawContent().substring(e.getMessage().getRawContent().indexOf("|") + 1)
-                            .trim().replace("\n", "\\n");
-
-                    if (name.isEmpty()) {
-                        chat.sendMessage("Your command's name cannot be null!");
-                        return;
-                    } else if (name.contains(" ")) {
-                        chat.sendMessage("Your command's name cannot contain spaces!");
-                        return;
-                    } else if (CommandRegistry.getCommand(name) != null) {
-                        chat.sendMessage("You cannot override the default commands! "
-                                + "**" + MessageUtil.stripFormatting(getPrefix(e.getGuild())) + "help " + name + "**");
-                        return;
-                    } else if (sm.isValid(name)) {
-                        chat.sendMessage("You already have a command registered by that name!");
-                        return;
-                    } else if (allResponses.isEmpty()) {
-                        chat.sendMessage("**Please include at least one response!**");
-                        return;
-                    } else if (allResponses.trim().equalsIgnoreCase("--private")) {
-                        chat.sendMessage("**Please include a message before the `Private` tag!**");
-                        return;
-                    }
-
-                    String[] responses = allResponses.split("(,(\\s+)?)");
-                    JSONObject obj = new JSONObject();
-                    obj.put("name", name);
-                    obj.put("responses", new JSONArray(responses));
-                    sm.addCommand(obj).update();
-                    chat.sendMessage(getSuccessMessage("created", name));
-                }
+                modifyCommand(args, e, chat, true);
                 break;
 
             case "list": // List either all commands or command info
                 StringBuilder sb = new StringBuilder();
-                sm.getCommands().entrySet().stream()
+                new ServerManager(e.getGuild()).getCommands().entrySet().stream()
                         .map(set -> "Command \"" + MessageUtil.stripFormatting(set.getKey()) + "\" - Responses " + set.getValue() + "\n")
                         .forEach(sb::append);
                 chat.sendMessage(sb.length() > 0 ? sb.toString() : "No custom commands have been registered!");
@@ -72,12 +33,32 @@ public class CustomCmdCommand extends GuildCommand {
 
             case "delete": // Delete a command
                 if (isNotAuthorized(chat, e.getAuthor(), e.getGuild())) return;
-                else if (inputArgs.isEmpty()) chat.sendUsageMessage();
+                String inputArgs = String.join(" ", Arrays.asList(Arrays.copyOfRange(args, 1, args.length)));
+                ServerManager sm = new ServerManager(e.getGuild());
+                if (inputArgs.isEmpty()) chat.sendUsageMessage();
                 else if (sm.isValid(inputArgs)) {
                     sm.deleteCommand(inputArgs).update();
                     chat.sendMessage(getSuccessMessage("deleted", inputArgs));
                 } else chat.sendMessage("That's not a valid command name!");
                 break;
+
+            case "update":
+            case "modify":
+            case "edit": // Edit existing command
+                modifyCommand(args, e, chat, false);
+                break;
+
+            case "reset":
+                if (isNotAuthorized(chat, e.getAuthor(), e.getGuild())) return;
+                Set<String> cmds = new ServerManager(e.getGuild()).getCommands().keySet();
+                if (cmds.isEmpty())
+                    chat.sendMessage("This guild has no custom commands!");
+                else {
+                    ServerManager manager = new ServerManager(e.getGuild());
+                    cmds.forEach(manager::deleteCommand);
+                    manager.update();
+                    chat.sendMessage("Deleted all custom commands! \uD83D\uDE35");
+                }
         }
     }
 
@@ -98,7 +79,7 @@ public class CustomCmdCommand extends GuildCommand {
 
     @Override
     public List<String> getRequiredParams() {
-        return Collections.singletonList("create / list / delete");
+        return Collections.singletonList("create / list / modify / delete / reset");
     }
 
     @Override
@@ -141,7 +122,56 @@ public class CustomCmdCommand extends GuildCommand {
         } else return false;
     }
 
+    private boolean missingArguments(String[] args, MessageSender chat) {
+        if (!String.join(" ", Arrays.asList(args)).contains("|")) {
+            chat.sendUsageMessage();
+            return true;
+        }
+        return false;
+    }
+
     private String getSuccessMessage(String action, String commandName) {
         return "Successfully " + action + " the command \"" + MessageUtil.stripFormatting(commandName) + "\"!";
+    }
+
+    private void modifyCommand(String[] args, MessageReceivedEvent e, MessageSender chat, boolean createNew) {
+        if (isNotAuthorized(chat, e.getAuthor(), e.getGuild()) || missingArguments(args, chat)) return;
+
+        String inputArgs = String.join(" ", Arrays.asList(Arrays.copyOfRange(args, 1, args.length)));
+        ServerManager sm = new ServerManager(e.getGuild());
+
+        String name = inputArgs.substring(0, inputArgs.indexOf("|")).trim();
+        String allResponses = e.getMessage().getRawContent()
+                .substring(e.getMessage().getRawContent().indexOf("|") + 1)
+                .trim().replace("\n", "\\n");
+
+        if (name.isEmpty()) {
+            chat.sendMessage("Your command's name cannot be null!");
+            return;
+        } else if (name.contains(" ")) {
+            chat.sendMessage("Your command's name cannot contain spaces!");
+            return;
+        } else if (CommandRegistry.getCommand(name) != null) {
+            chat.sendMessage("You cannot override the default commands! "
+                    + "**" + MessageUtil.stripFormatting(getPrefix(e.getGuild())) + "help " + name + "**");
+            return;
+        } else if (createNew && sm.isValid(name)) {
+            chat.sendMessage("You already have a command registered by that name!");
+            return;
+        } else if (allResponses.isEmpty()) {
+            chat.sendMessage("**Please include at least one response!**");
+            return;
+        } else if (allResponses.trim().equalsIgnoreCase("--private")) {
+            chat.sendMessage("**Please include a message before the `Private` tag!**");
+            return;
+        }
+
+        String[] responses = allResponses.split("(,(\\s+)?)");
+        JSONObject obj = new JSONObject();
+        obj.put("name", name);
+        obj.put("responses", new JSONArray(responses));
+        if (!createNew) sm.deleteCommand(name);
+        sm.addCommand(obj).update();
+        chat.sendMessage(getSuccessMessage((createNew ? "created" : "updated"), name));
     }
 }
