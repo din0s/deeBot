5af227fab8b14a5ac54767d6fac4a870ff2c0d11
Dinos <emeraldpmc@gmail.com>
2016-10-28 19:58:29 +0300

    Added Case Sensitive option and improved flag parsing

--- src/main/java/me/dinosparkour/commands/guild/UserSearchCommand.java
+++ src/main/java/me/dinosparkour/commands/guild/UserSearchCommand.java
@@ -12,24 +12,41 @@ public class UserSearchCommand extends GuildCommand {
 
     @Override
     public void executeCommand(String[] args, MessageReceivedEvent e, MessageSender chat) {
-        String allArgs = String.join(" ", Arrays.asList(args));
-        boolean includeNicks = args[args.length - 1].equalsIgnoreCase("--N");
-        boolean isGlobal = args[args.length - 1].equalsIgnoreCase("--G");
+        List<String> argArray = Arrays.asList(args);
+        String allArgs = String.join(" ", argArray);
+        Set<String> flags = argArray.stream().filter(arg -> arg.startsWith("-")).map(String::toLowerCase).collect(Collectors.toSet());
+        boolean includeNicks = flags.contains("-n");
+        boolean isGlobal = flags.contains("-g");
+        boolean isCaseSensitive = flags.contains("--case-sensitive");
         List<User> baseCollection = isGlobal ? e.getJDA().getUsers() : e.getGuild().getUsers();
-        String query = isGlobal || includeNicks ? allArgs.substring(0, allArgs.length() - 4) : allArgs;
+        String query = allArgs.replaceAll(" (?i)-n", "")
+                .replaceAll(" (?i)-g", "")
+                .replaceAll(" (?i)--case-sensitive", "").trim();
+
         List<String> results = baseCollection.stream()
                 .filter(u -> {
-                    String nick = e.getGuild().getNicknameForUser(u);
-                    String sample = includeNicks ? nick == null ? u.getUsername() : nick : u.getUsername();
-                    return sample.toLowerCase().contains(query.toLowerCase());
-                })
-                .map(MessageUtil::userDiscrimSet)
+                    if (isGlobal && includeNicks)
+                        return e.getJDA().getGuilds().stream().anyMatch(g -> {
+                            String nick = g.getNicknameForUser(u);
+                            String name = isCaseSensitive ? query : query.toLowerCase();
+                            if (nick == null) return u.getUsername().contains(name);
+
+                            if (!isCaseSensitive) nick = nick.toLowerCase();
+                            return nick.contains(name);
+                        });
+                    else {
+                        String nick = e.getGuild().getNicknameForUser(u);
+                        String sample = includeNicks ? nick == null ? u.getUsername() : nick : u.getUsername();
+                        if (!isCaseSensitive) sample = sample.toLowerCase();
+                        return sample.contains(isCaseSensitive ? query : query.toLowerCase());
+                    }
+                }).map(MessageUtil::userDiscrimSet)
                 .collect(Collectors.toList());
 
         if (results.size() > 20)
             chat.sendMessage("Your query returned " + results.size() + " users! Please narrow down your search.");
         else if (results.size() == 0)
-            chat.sendMessage("Your query returned no results!");
+            chat.sendMessage("Your query returned no results! (You searched for _" + MessageUtil.stripFormatting(query) + "_)");
         else
             chat.sendMessage((isGlobal ? "Global " : "") + (includeNicks ? "Nicknamed " : "")
                     + "Users matching \"" + MessageUtil.stripFormatting(query) + "\":\n" + String.join("\n", results));
@@ -57,9 +74,10 @@ public class UserSearchCommand extends GuildCommand {
 
     @Override
     public Map<String, String> getFlags() {
-        Map<String, String> flags = new HashMap<>();
-        flags.put("--N", "Include nicknames");
-        flags.put("--G", "Perform global search");
+        Map<String, String> flags = new LinkedHashMap<>();
+        flags.put("-N", "Include nicknames");
+        flags.put("-G", "Perform global search");
+        flags.put("--case-sensitive", "Strictly match Case Sensitivity");
         return flags;
     }
 
