dc6f2529464b80f0f36043370ad2b87d799e774a
Dinos <emeraldpmc@gmail.com>
2016-10-28 18:39:11 +0300

    Fixed an IllegalArgumentException thrown by regex Matcher

--- src/main/java/me/dinosparkour/utils/MessageUtil.java
+++ src/main/java/me/dinosparkour/utils/MessageUtil.java
@@ -14,6 +14,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
+import java.util.regex.Matcher;
 import java.util.stream.Collectors;
 
 public class MessageUtil {
@@ -94,6 +95,9 @@ public class MessageUtil {
 
     public static String parseVariables(String message, User user) {
         return message.replaceAll("(?i)%userid%", user.getId())
-                .replaceAll("(?i)%user%", stripFormatting(user.getUsername()));
+                .replaceAll("(?i)%user%", stripFormatting(user.getUsername()
+                        .replace("\\", Matcher.quoteReplacement("\\"))
+                        .replace("$", Matcher.quoteReplacement("$"))
+                ));
     }
 }
