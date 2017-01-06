927deb9d83517b107d4974686416f8473ec41046
Dinos <emeraldpmc@gmail.com>
2017-01-06 04:30:11 +0200

    Update to 2.3.7

--- src/main/java/me/dinosparkour/Info.java
+++ src/main/java/me/dinosparkour/Info.java
@@ -8,12 +8,12 @@ import java.lang.management.ManagementFactory;
 public class Info {
 
     public static final String DEFAULT_PREFIX = "!!";
-    public static final String VERSION = "2.3.6";
+    public static final String VERSION = "2.3.7";
     public static final String LIBRARY = "JDA #" + JDAInfo.VERSION;
     public static final String AUTHOR_ID = "98457903660269568";
     public static final String BOT_ID = "137904252214444032";
 
-    private static final Config CONFIG = new Config();                 // config.json
+    private static final Config CONFIG = new Config();                      // config.json
     public static final String ABAL_KEY = CONFIG.getValue("abal");     // https://bots.discord.pw
     public static final String CARBON_KEY = CONFIG.getValue("carbon"); // https://www.carbonitex.net/discord/bots
     public static final String GOOGLE_CX = CONFIG.getValue("cx");      // https://cse.google.com
