b00a04f4d5ab543b84a0af9f8e7a0eaffcf3c179
Dinos <dinospk1999@gmail.com>
2017-09-12 16:17:02 +0300

    Now using WS ping

--- src/main/java/me/dinosparkour/commands/global/PingCommand.java
+++ src/main/java/me/dinosparkour/commands/global/PingCommand.java
@@ -3,7 +3,6 @@ package me.dinosparkour.commands.global;
 import me.dinosparkour.commands.impls.GlobalCommand;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
-import java.time.temporal.ChronoUnit;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,10 +10,7 @@ public class PingCommand extends GlobalCommand {
 
     @Override
     public void executeCommand(String[] args, MessageReceivedEvent e, MessageSender chat) {
-        chat.sendMessage("Pinging..", m -> {
-            long diff = e.getMessage().getCreationTime().until(m.getCreationTime(), ChronoUnit.MILLIS);
-            m.editMessage("Ping: **" + (diff / 2) + "ms**").queue();
-        });
+        chat.sendMessage(("WebSocket Ping: **" + e.getJDA().getPing() / 2 + "ms**"));
     }
 
     @Override
